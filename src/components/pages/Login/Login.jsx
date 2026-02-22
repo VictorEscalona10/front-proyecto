affb309ebdcd737aa6b99c9107b1a9ed85d1b2fd
Victor Escalona <victorescalona2006@gmail.com>
2026-02-21 22:51:18 -0400

    prueba

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -63,7 +63,7 @@ export default function Login({ onShowModal }) {
     try {
       console.log("Enviando request a:", `${API_URL}/auth/login`);
       
-      const response = await fetch(`${API_URL}/auth/login`, {
+      const response = await fetch(`${API_URL}auth/login`, {
         method: "POST",
         headers: { 
           "Content-Type": "application/json",
